f9e5927d1f9231dac44dcfa5f7ae1e2651b04d55
Hooz-torder <ksh4@torder.com>
2024-05-30 15:20:03 +0900

    wip

--- .github/workflows/random-reviewer-action.ts
+++ .github/workflows/random-reviewer-action.ts
@@ -4,26 +4,66 @@ import * as fs from "fs";
 import * as path from "path";
 import * as yaml from "js-yaml";
 
+interface Reviewer {
+  githubName: string;
+  slackUserId: string;
+}
+
 // YAML 파일 경로 설정
 const yml = path.join(__dirname, "../../", "reviewer.yml");
 
 function main() {
-  console.log("random job step start", context);
+  console.log("random job step start");
+
   const token = getInput("github_token");
-  console.log("@@@@", token);
+  const githubClient = getOctokit(token);
+
   const { reviewers } = getReviewers(yml) as {
-    reviewers: { githubName: string; slackUserId: string }[];
+    reviewers: Reviewer[];
   };
 
-  if (reviewers.length > 0) {
-    // await githubClient.rest.pulls.requestReviewers({
-    //   owner: github.context.repo.owner,
-    //   repo: github.context.repo.repo,
-    //   pull_number: github.context.issue.number,
-    //   reviewers: [selectedReviewer.githubName],
-    // });
+  const creator = context?.payload?.pull_request?.user.login;
+  let candidates = reviewers.filter((reviewer) => {
+    return true;
+    return reviewer.githubName !== creator;
+  });
+
+  if (candidates.length > 0) {
+    const firstReviewer = getRandomReviewer(candidates);
+    candidates.splice(firstReviewer.index, 1);
+    const secondReviewer = getRandomReviewer(candidates);
+
+    [firstReviewer, secondReviewer].forEach(async (reviewer) => {
+      await githubClient.rest.pulls.requestReviewers({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.issue.number,
+        reviewers: [reviewer.githubName],
+      });
+
+      sendDirectMessage({
+        githubName: reviewer.githubName,
+        slackUserId: reviewer.slackUserId,
+      });
+    });
   }
 }
+
+const sendDirectMessage = async (reviewer: Reviewer) => {
+  // slackClient.chat.postMessage({
+  //   text: createMessage(github.context),
+  //   channel: reviewer.slackUserId,
+  // });
+};
+
+const getRandomReviewer = (candidates: Reviewer[]) => {
+  const memberIndex = Math.floor(Math.random() * candidates.length);
+  return {
+    githubName: candidates[memberIndex].githubName,
+    slackUserId: candidates[memberIndex].slackUserId,
+    index: memberIndex,
+  };
+};
 
 // YAML 파일 읽기
 const getReviewers = (filePath: string) => {
